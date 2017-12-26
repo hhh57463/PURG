b66eab23dbd281b635973240c1a2379d4210887d
hhh57463 <hhh57463@naver.com>
2017-12-26 12:19:28 +0900

    Player Temporary Ani, Item Rezen

---
+++ Assets/Resource/GameScene/Script/Item.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    public BoxCollider ItemBc = null;
+    public SpriteRenderer ItemSr = null;
+
+    public bool bItemMoveAllow = false;
+    float fItemMoveSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        fItemMoveSpeed = 3.0f;
+        bItemMoveAllow = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ItemMove();
+	}
+
+    void ItemMove()
+    {
+        if (!SGameMng.I.bPlayerDie && !bItemMoveAllow)
+        {
+            transform.Translate(Vector2.left * fItemMoveSpeed * Time.deltaTime);
+        }
+
+        if (transform.localPosition.x <= -25f) 
+        {
+            StartCoroutine(SGameMng.I.ItemSpawn());
+            SGameMng.I.bItemAllow = false;
+            SGameMng.I.nItemRand = 0;
+            bItemMoveAllow = true;
+            transform.localPosition = Vector3.zero;
+            ItemBc.enabled = true;
+            ItemSr.enabled = true;
+            //Destroy(gameObject);
+            
+        }
+    }
+
+}
---
+++ Assets/Resource/GameScene/Script/SGameMng.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SGameMng : MonoBehaviour
+{
+
+    private static SGameMng _Instance = null;
+
+    public Item ItemSc = null;
+
+    public bool bPlayerDie = false;
+    public bool bItemAllow = false;
+
+    public int nItemRand = 0;
+
+    public static SGameMng I
+    {
+        get
+        {
+            if (_Instance.Equals(null))
+            {
+                Debug.Log("instance is null");
+            }
+            return _Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _Instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ItemSpawn());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+
+                if (hit.transform.CompareTag("Item"))
+                {
+                    Debug.Log("이곳에서 아이템 클릭시 하는 이벤트 발생");
+                }
+            }
+        }
+
+        if (nItemRand == 0)
+        {
+
+        }
+        else if (nItemRand % 2 == 0)                     //짝수
+        {
+            bItemAllow = true;
+            ItemSc.bItemMoveAllow = false;
+            //nItemRand = 0;
+        }
+        else/* if (nItemRand & 2 == 1)*/            //홀수
+        {
+            bItemAllow = false;
+            //nItemRand = 0;
+            ItemSc.bItemMoveAllow = false;
+            ItemSc.ItemBc.enabled = false;
+            ItemSc.ItemSr.enabled = false;
+        }
+
+    }
+
+    public IEnumerator ItemSpawn()
+    {
+        Debug.Log("코루틴 시작!");
+        yield return new WaitForSeconds(5f);                                //여기 이후가 다시 호출이 안됨 Item스크립트에서 Item게임오브젝트를 Destroy해줘 코루틴이 끊기는듯
+        if (!bPlayerDie)
+        {
+            nItemRand = Random.Range(1, 10);                         //1이면 아이템 출현 2면 X
+            Debug.Log("랜덤값 지정 완료!");
+            //StartCoroutine(ItemSpawn());
+        }
+    }
+
+}
